ae2dda86b0e39b05bdafe604bda0fe21673845ba
Dan Michaelis <dmichaelis0+github@gmail.com>
2022-02-07 20:34:45 -0500

    add vercel and tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,20 @@
+const formsPlugin = require( "@tailwindcss/forms" );
+
 module.exports = {
-    darkMode: false,
-    mode: 'jit',
-    plugins: [],
-    purge: ['./src/**/*.jsx', './src/**/*.js', './public/index.html'],
-    // or 'media' or 'class'
-    theme: {
-        extend: {},
-        fontFamily: {
-            body: ['Roboto', 'sans-serif'],
-            sans: ['Roboto', 'sans-serif'],
-            serif: ['"Roboto Slab"', 'serif'],
-        },
-    },
-    variants: {
-        extend: {},
-    },
+	darkMode: false,
+	mode:     'jit',
+	plugins:  [ formsPlugin ],
+	purge:    [ './src/**/*.jsx', './src/**/*.js', './public/index.html' ],
+	// or 'media' or 'class'
+	theme:    {
+		extend:     {},
+		fontFamily: {
+			body:  [ 'Roboto', 'sans-serif' ],
+			sans:  [ 'Roboto', 'sans-serif' ],
+			serif: [ '"Roboto Slab"', 'serif' ]
+		}
+	},
+	variants: {
+		extend: {}
+	}
 };
